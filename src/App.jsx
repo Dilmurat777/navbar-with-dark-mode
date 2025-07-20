dd9e174ec96d6d5b9c4c042ab6bbf05418aac190
user.email <mrdilmurat777@gmail.com>
2025-07-20 20:08:05 +0600

    feat: add toggle button to navbar dark and light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-
+import { useEffect, useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const current_them = localStorage.getItem('current_them');
+  const [them, setThem] = useState(current_them ? current_them : 'light');
 
-export default App
+  useEffect(() => { 
+    localStorage.setItem('current_them', them)
+  }, [them])
+  return (
+    <div className={`container ${them}`}>
+      <Navbar them={them} setThem={setThem} />
+    </div>
+  );
+};
+
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,34 @@
-import './Navbar.css'
+import './Navbar.css';
+import logo_light from '../../assets/logo-black.png';
+import logo_dark from '../../assets/logo-white.png';
+import search_icon_light from '../../assets/search-w.png';
+import search_icon_dark from '../../assets/search-b.png';
+import toggle_light from '../../assets/night.png';
+import toggle_dark from '../../assets/day.png';
 
-const Navbar = () => {
+const Navbar = ({ them, setThem }) => {
+	const toggle = () => {
+		them == 'light' ? setThem('dark') : setThem('light')
+	}
   return (
-	<div>
-	  
-	</div>
-  )
-}
+    <div className={`navbar`}>
+      <img src={them === 'light' ? logo_light : logo_dark} alt="" className="logo" />
 
-export default Navbar
+      <ul>
+        <li>Home</li>
+        <li>Products</li>
+        <li>Features</li>
+        <li>About</li>
+      </ul>
+
+      <div className="search-box">
+        <input type="text" placeholder="Search" />
+        <img src={them === 'light' ? search_icon_light : search_icon_dark} alt="" />
+      </div>
+
+      <img onClick={() => toggle()} src={them === 'light' ? toggle_light : toggle_dark} alt="" className="toggle-icon" />
+    </div>
+  );
+};
+
+export default Navbar;
